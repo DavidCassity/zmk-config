42feaaa3dbacb17336eda51f5f23e97e6acf5c75
DavidCassity <davitrton123@gmail.com>
2024-04-14 15:47:17 -0500

    messed up the helper position

--- config/boards/shields/zesty/zesty32keys.h
+++ config/boards/shields/zesty/zesty32keys.h
@@ -5,37 +5,37 @@
 #define LT2  1
 #define LT3  0
 
-#define RT0  19  // right-top row
-#define RT1  18
-#define RT2  17
-#define RT3  16
+#define RT0  4  // right-top row
+#define RT1  5
+#define RT2  6
+#define RT3  7
 
-#define LM0 9  // left-middle row
-#define LM1 8
-#define LM2 7
-#define LM3 6
-#define LM4 5
-#define LM5 4
+#define LM0 13  // left-middle row
+#define LM1 12
+#define LM2 11
+#define LM3 10
+#define LM4 9
+#define LM5 8
 
-#define RM0 25  // right-middle row
-#define RM1 24
-#define RM2 23
-#define RM3 22
-#define RM4 21
-#define RM5 20
+#define RM0 14  // right-middle row
+#define RM1 15
+#define RM2 16
+#define RM3 17
+#define RM4 18
+#define RM5 19
 
-#define LB0 13  // left-bottom row
-#define LB1 12
-#define LB2 11
-#define LB3 10
+#define LB0 23  // left-bottom row
+#define LB1 22
+#define LB2 21
+#define LB3 20
 
-#define RB0 29  // right-bottom row
-#define RB1 28
-#define RB2 27
-#define RB3 26
+#define RB0 24  // right-bottom row
+#define RB1 25
+#define RB2 26
+#define RB3 27
 
-#define LH0 15  // left thumb keys
-#define LH1 14
+#define LH0 29  // left thumb keys
+#define LH1 28
 
-#define RH0 31  // right thumb keys
-#define RH1 30
+#define RH0 30  // right thumb keys
+#define RH1 31
